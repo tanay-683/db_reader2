618d517ffe5806bc191ad7fae991063efdeb760e
tanay-683 <tanay.r.sharma@gmail.com>
2024-11-25 22:30:19 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+project_name = "db_reader"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/model",
+    f"src/{project_name}/model/__init__.py",
+    f"src/{project_name}/model/model.py",
+    
+    f"src/{project_name}/sql_connection",
+    f"src/{project_name}/sql_connection/__init__.py",
+    f"src/{project_name}/sql_connection/connection.py",
+    
+    f"templates",
+    f"research",
+    "main.py",
+    
+]
